f102adf3883aafb3d95b4b35ec28063f424f1853
bkz7354 <biglov.kz@phystech.edu>
2020-10-22 16:12:06 +0300

    aded comments

--- queens_v2.cpp
+++ queens_v2.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <type_traits>
 
 
-
-// used to iterate
-template<int Sz, int Pos = 0>
+// used to iterate over a range 0, ..., (Sz-1)
+template<int Sz, int Pos = 0, typename Enable = void>
 struct range{
     using next = range<Sz, Pos + 1>;
-    const static bool is_end = (Pos >= Sz);
+    const static bool is_end = false;
     const static int pos = Pos;
 };
 
+// partial speciliztion to detect end of range
+template<int Sz, int Pos>
+struct range<Sz, Pos, typename std::enable_if<(Pos >= Sz)>::type>{
+	const static bool is_end = true;
+};
+
+// actually checks the layout
 template<int X, int Y, int Y2, int ...List>
 struct check_attack_helper;
 
@@ -24,18 +31,29 @@ struct check_attack_helper<X, Y, Y2, X2, List...>{
 };
 
 
-// returns false if layout is imposible
+// returns false if layout is imposible 
+// (X, Y) - the position of the current queen
+// List - x coordinates of previous queens
 template<int X, int Y, int ...List>
 struct check_attack{
     static const bool value = check_attack_helper<X, Y, 0, List...>::value;
 };
 
 
+// iterates over a row by using the range struct passed as X
+// uses the B variable to check if the row ended
 template<int N, bool B, typename X, typename Y, int ...XS>
 struct pass_row;
 
+// checks if the current row is the past the last (using the B variable), starts pass_row otherwise
 template<int N, bool B, typename Y, int ...XS>
 struct start_row;
+
+// checks_if layout is possible (using the B variable), if it is, starts next row
+template<int N, bool B, typename Y, int ...XS>
+struct init_next_row;
+
+
 
 template<int N, typename Y, int ...XS>
 struct start_row<N, false, Y, XS...>{
@@ -48,15 +66,11 @@ struct start_row<N, true, Y, XS...>{
 };
 
 
-template<int N, bool B, typename Y, int ...XS>
-struct init_next_row;
-
 
 template<int N, typename Y, int ...XS>
 struct init_next_row<N, false,Y, XS...>{
     static const int value = 0;
 };
-
 
 template<int N, typename Y, int ...XS>
 struct init_next_row<N, true,Y, XS...>{
@@ -64,11 +78,11 @@ struct init_next_row<N, true,Y, XS...>{
 };
 
 
+
 template<int N, typename X, typename Y, int ...XS>
 struct pass_row<N, false, X, Y, XS...>{
     static const int value = 0;
 };
-
 
 template<int N, typename X, typename Y, int ...XS>
 struct pass_row<N, true, X, Y, XS...>{
